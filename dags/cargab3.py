adefcb15f2a25cc7cdf3ea6cc3628082f6a294d8
Yan Corrêa <yansouza009@gmail.com>
2024-11-05 01:47:47 -0300

    fix: correcao load precos

--- dags/cargab3.py
+++ dags/cargab3.py
@@ -11,62 +11,68 @@ def get_db_connection():
     hook = PostgresHook(postgres_conn_id='postgres_default')
     return hook.get_conn()
 
-def limpar_dados(coluna):
-    """Limpa a coluna e a converte para string.""" 
-    coluna = coluna.astype(str).str.replace('R\$', '', regex=True).str.strip()
-    coluna = coluna.str.replace('.', '', regex=True)
-    coluna = coluna.str.replace(',', '.', regex=True)
-    return coluna
-
 def extract():
     logging.info("Iniciando a extração...")
-    arquivo = '/opt/airflow/files/COTAHIST_A2024.txt'
-    logging.info("Caminho absoluto do arquivo: %s", os.path.abspath(arquivo))
+    anos = ['2022', '2023', '2024']
+    arquivos = [f'/opt/airflow/files/COTAHIST_A{ano}.txt' for ano in anos]
     
-    separar_campos = [2, 8, 2, 12, 3, 12, 4, 13, 13, 13, 13, 13, 13, 13, 5, 18, 18, 12, 3]
+    logging.info("Caminhos absolutos dos arquivos: %s", [os.path.abspath(arquivo) for arquivo in arquivos])
+    
+    separar_campos = [2, 8, 2, 12, 3, 12, 10, 3, 4, 13, 13, 13, 13, 13, 13, 13, 5, 18, 18, 13, 1, 8, 7, 13, 12, 3]
     colunas = [
-        "tipo_registro", "data_pregao", "cod_bdi", "cod_negociacao", "tipo_mercado",
-        "nome_empresa", "moeda", "preco_abertura", "preco_maximo", "preco_minimo",
-        "preco_medio", "preco_ultimo_negocio", "preco_melhor_oferta_compra",
-        "preco_melhor_oferta_venda", "numero_negocios", "quantidade_papeis_negociados",
-        "volume_total_negociado", "codigo_isin", "num_distribuicao_papel"
+        "tipo_registro", "data_pregao", "cod_bdi", "cod_negociacao", "tipo_mercado", 
+        "nome_empresa", "especificacao_papel", "prazo_dias_merc_termo", "moeda", 
+        "preco_abertura", "preco_maximo", "preco_minimo", "preco_medio", 
+        "preco_ultimo_negocio", "preco_melhor_oferta_compra", "preco_melhor_oferta_venda", 
+        "numero_negocios", "quantidade_papeis_negociados", "volume_total_negociado", 
+        "preco_exercicio", "indicador_correcao_precos", "data_vencimento", 
+        "fator_cotacao", "preco_exercicio_pontos", "codigo_isin", "num_distribuicao_papel"
     ]
-    
-    acoes = ['PETR4', 'VALE3', 'MGLU3', 'RAIZ4', 'AMBP3', 'B3SA3', 'ABEV3', 'ITUB4', 'WEGE3']
+    acoes = ['PETR4', 'VALE3', 'MGLU3', 'RAIZ4', 'AMBP3', 'B3SA3', 'ABEV3', 'ITUB4', 'WEGE3', 'BBDC4', 'QUAL3']
 
     todos_dados = []
-
     chunk_size = 10000
-    for chunk in pd.read_fwf(arquivo, widths=separar_campos, header=0, chunksize=chunk_size):
-        logging.info("Lendo um bloco de dados...")
-        
-        if chunk.shape[1] != len(colunas):
-            logging.error("Número de colunas lidas (%d) não corresponde ao número de colunas definidas (%d)", 
-                          chunk.shape[1], len(colunas))
-            raise ValueError("Número de colunas não compatível.")
+    
+    try:
+        for arquivo in arquivos:
+            logging.info("Processando arquivo: %s", arquivo)
+            
+            if not os.path.isfile(arquivo):
+                logging.error("Arquivo não encontrado: %s", arquivo)
+                continue
 
-        chunk.columns = colunas
-        chunk = chunk[chunk['cod_negociacao'].isin(acoes)]
-        logging.info("Número de registros filtrados neste bloco: %d", chunk.shape[0])
+            for chunk in pd.read_fwf(arquivo, widths=separar_campos, header=0, chunksize=chunk_size):
+                logging.info("Lendo um bloco de dados...")
+                
+                chunk.columns = colunas
+                
+                chunk = chunk[chunk['cod_negociacao'].isin(acoes)]
+                logging.info("Número de registros filtrados neste bloco: %d", chunk.shape[0])
 
-        colunas_a_limpar = [
-            "preco_abertura",
-            "preco_maximo",
-            "preco_minimo",
-            "preco_medio",
-            "preco_ultimo_negocio",
-            "preco_melhor_oferta_compra",
-            "preco_melhor_oferta_venda"
-        ]
+                dinheiro = [
+                    "preco_abertura", "preco_maximo", "preco_minimo", 
+                    "preco_medio", "preco_ultimo_negocio", 
+                    "preco_melhor_oferta_compra", "preco_melhor_oferta_venda"
+                ]
 
-        for coluna in colunas_a_limpar:
-            if coluna in chunk.columns:
-                chunk[coluna] = limpar_dados(chunk[coluna])
+                for coluna in dinheiro:
+                    if coluna in chunk.columns:
+                        logging.info("Antes da divisão - %s: %s", coluna, chunk[coluna].head())
+                        chunk[coluna] = chunk[coluna].astype(float) / 100
+                        logging.info("Depois da divisão - %s: %s", coluna, chunk[coluna].head())
 
-        todos_dados.extend(chunk.to_dict(orient='records'))
+                chunk['data_pregao'] = pd.to_datetime(chunk['data_pregao'], format='%Y%m%d', errors='coerce')
+                chunk['data_pregao'] = chunk['data_pregao'].dt.strftime('%d/%m/%Y')
 
-    logging.info("Total de registros extraídos: %d", len(todos_dados))
-    return todos_dados
+                todos_dados.extend(chunk.to_dict(orient='records'))
+
+        logging.info("Total de registros extraídos: %d", len(todos_dados))
+        return todos_dados
+
+    except Exception as e:
+        logging.error("Erro na extração: %s", e)
+        return []
+
 
 def load_stage(data):
     conn = get_db_connection()
@@ -98,6 +104,7 @@ def load_stage(data):
             num_distribuicao_papel
         ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
         """
+        
         if data:
             logging.info("Dados a serem inseridos: %s", data)
 
@@ -110,13 +117,13 @@ def load_stage(data):
                     str(record['tipo_mercado']),
                     str(record['nome_empresa']),
                     str(record['moeda']),
-                    str(record['preco_abertura']),
-                    str(record['preco_maximo']),
-                    str(record['preco_minimo']),
-                    str(record['preco_medio']),
-                    str(record['preco_ultimo_negocio']),
-                    str(record['preco_melhor_oferta_compra']),
-                    str(record['preco_melhor_oferta_venda']),
+                    float(record['preco_abertura']),
+                    float(record['preco_maximo']),
+                    float(record['preco_minimo']),
+                    float(record['preco_medio']),
+                    float(record['preco_ultimo_negocio']),
+                    float(record['preco_melhor_oferta_compra']),
+                    float(record['preco_melhor_oferta_venda']),
                     str(record['numero_negocios']),
                     str(record['quantidade_papeis_negociados']),
                     str(record['volume_total_negociado']),
@@ -143,15 +150,15 @@ def load_stage(data):
 def insert_calendar_data(conn):
     """Insere dados na tabela de calendário, convertendo data_pregao para DATE."""
     with conn.cursor() as cursor:
-        cursor.execute("""
+        cursor.execute(""" 
         INSERT INTO dim_calendario (data, dia, mes, ano, trimestre, dia_da_semana)
         SELECT DISTINCT 
-            TO_DATE(data_pregao, 'YYYYMMDD') AS data, 
-            EXTRACT(DAY FROM TO_DATE(data_pregao, 'YYYYMMDD')),
-            EXTRACT(MONTH FROM TO_DATE(data_pregao, 'YYYYMMDD')),
-            EXTRACT(YEAR FROM TO_DATE(data_pregao, 'YYYYMMDD')),
-            EXTRACT(QUARTER FROM TO_DATE(data_pregao, 'YYYYMMDD')),
-            TO_CHAR(TO_DATE(data_pregao, 'YYYYMMDD'), 'Day')
+            TO_DATE(data_pregao, 'DD/MM/YYYY') AS data, 
+            EXTRACT(DAY FROM TO_DATE(data_pregao, 'DD/MM/YYYY')),
+            EXTRACT(MONTH FROM TO_DATE(data_pregao, 'DD/MM/YYYY')),
+            EXTRACT(YEAR FROM TO_DATE(data_pregao, 'DD/MM/YYYY')),
+            EXTRACT(QUARTER FROM TO_DATE(data_pregao, 'DD/MM/YYYY')),
+            TO_CHAR(TO_DATE(data_pregao, 'DD/MM/YYYY'), 'Day')
         FROM stage_cotahist
         WHERE data_pregao IS NOT NULL AND data_pregao <> ''
         ON CONFLICT (data) DO NOTHING;
@@ -177,7 +184,7 @@ def transform_and_enrich():
             SELECT da.id, dc.id, sc.preco_abertura, sc.preco_ultimo_negocio, sc.volume_total_negociado 
             FROM stage_cotahist sc 
             JOIN dim_ativos da ON sc.cod_negociacao = da.ativo
-            JOIN dim_calendario dc ON TO_DATE(sc.data_pregao, 'YYYYMMDD') = dc.data;
+            JOIN dim_calendario dc ON TO_DATE(sc.data_pregao, 'DD/MM/YYYY') = dc.data;
             """)
 
         conn.commit()
